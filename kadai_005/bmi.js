6bd2a44c1a126a33101a66dd271e6de2d6923d23
hirohikotakezoe <naviestokes7171@gmail.com>
2024-09-20 14:45:06 +0900

    課題005提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の定義
+
+
+let bodyWeight;
+let bodyHeight;
+let bodyBmi;
+// 変数の代入
+bodyWeight=68;
+bodyHeight=1.7;
+
+// 計算
+bodyBmi=bodyWeight/bodyHeight/bodyHeight;
+// 出力
+console.log(bodyBmi);
