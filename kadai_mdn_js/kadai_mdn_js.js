77ef42040e87ae1fdc8405975c4e8bf3d45345ef
hirohikotakezoe <naviestokes7171@gmail.com>
2024-10-06 16:23:04 +0900

    年月日表示

--- kadai_mdn_js/kadai_mdn_js.js
+++ kadai_mdn_js/kadai_mdn_js.js
@@ -1,3 +1,6 @@
-let now
-now = new Date()
-console.log(now)
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+
+console.log(year + '年' + month + '月' + day + '日');
